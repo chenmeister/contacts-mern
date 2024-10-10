a1cfc30e6ecf4897fc9bdd290a98dbf5eb74c2f3
Sean Chen <sean.chen909@gmail.com>
2024-10-09 22:17:20 -0700

    pushing in client code

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <nav></nav>
+        <h1>Contacts List</h1>
+      </div>
+      <div>
+        <button>Add Contact</button>
+      </div>
+    </>
+  )
+}
+
+export default App
