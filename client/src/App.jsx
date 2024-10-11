a346059863ed79e864777e9b1576e19245a105fd
Sean Chen <sean.chen909@gmail.com>
2024-10-11 16:53:08 -0700

    added more frontend components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,36 @@
+import AddContact from './pages/addContact'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <div>
-        <nav></nav>
-        <h1>Contacts List</h1>
-      </div>
-      <div>
-        <button>Add Contact</button>
+      <div className="container">
+        <nav>
+          <div>
+            <h1>Contacts List</h1>
+          </div>
+        </nav>
+
+        <div className="contacts_list">
+          <div className="right">
+            <button>Add Contact</button>
+          </div>
+          <table>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th></th>
+            </tr>
+            <tr>
+              <td>Test</td>
+              <td>User</td>
+              <td>Edit, remove</td>
+            </tr>
+          </table>
+        </div>
+
+        <AddContact />
       </div>
     </>
   )
---
+++ client/src/pages/addContact/index.jsx
@@ -0,0 +1,21 @@
+
+const AddContact = () => {
+  return (
+    <>
+      <h3>Add Contact</h3>
+      <form>
+        <div>
+          <label>First Name</label>
+          <input type="text"/>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input type="text"/>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  )
+};
+
+export default AddContact;
