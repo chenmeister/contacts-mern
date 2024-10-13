bf32640fbbcac61459977947262296a5e575020d
Sean Chen <sean.chen909@gmail.com>
2024-10-13 11:11:33 -0700

    backend side done

---
+++ server/routes/contact.js
@@ -0,0 +1,82 @@
+import express from "express";
+
+// This will help us connect to the database
+import db from "../db/connection.js";
+
+// This help convert the id from string to ObjectId for the _id.
+import { ObjectId } from "mongodb";
+
+// router is an instance of the express router.
+// We use it to defined our routes.
+// The router will be added as a middleware and will take control of the requests starting 
+// with the path/record
+const router = express.Router();
+
+// This section will help you get a list of all the records.
+router.get("/", async (req, res) => {
+    let collection = await db.collection("contacts");
+    let results = await collection.find({}).toArray();
+    res.send(results).status(200);
+});
+
+// This section will help you get a single record by id
+router.get("/:id", async (req, res) => {
+    let collection = await db.collection("contacts");
+    let query = {_id: new ObjectId(req.params.id)};
+    let result = await collection.findOne(query);
+
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+});
+
+// This section will help you create a new record.
+router.post("/", async (req, res) => {
+    try {
+        let newDocument = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+        }
+        let collection = await db.collection("contacts");
+        let result = await collection.insertOne(newDocument);
+        res.send(result).status(204);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error adding contact");
+    }
+});
+
+// This section will help you update a record by id
+router.patch("/:id", async (req, res) => {
+    try {
+        const query = { _id: new ObjectId(req.params.id) };
+        const updates = {
+            $set: {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+            }
+        }
+        let collection = await db.collection("contacts");
+        let result = await collection.updateOne(query, updates);
+        res.send(result).status(200);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error updating contact");
+    }
+});
+
+// This seciton will help you delete a record
+router.delete("/:id", async (req, res) => {
+    try {
+        const query = { _id: new ObjectId(req.params.id) };
+
+        const collection = db.collection("contacts");
+        let result = await collection.deleteOne(query);
+
+        res.send(result).status(200);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error deleting contact");
+    }
+})
+
+export default router;
